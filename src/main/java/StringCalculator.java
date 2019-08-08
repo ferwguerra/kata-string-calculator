97f95795c608404bae908e2a6854d03650356f01
Fernando Guerra <fernandoguerra@Fernandos-MacBook-Pro.local>
2019-08-08 10:23:28 +0200

    Step 5 working: disallow negatives

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,32 +1,53 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringCalculator {
 
     private String separators = ",|\\n";
 
     public int add(String numbers) {
         int result = 0;
-        if(!numbers.isEmpty()) {
-
-            numbers = calculateSeparatorsCustom(numbers);
-
-            String[] arrayNumbers = numbers.replaceAll("\n", ",").split(separators);
-            for (String number : arrayNumbers) {
-                result += Integer.valueOf(number);
-            }
-
-
-
+        if (numbers.isEmpty()) {
+            return result;
         }
+
+        List<Integer> negatives = new ArrayList<Integer>();
+        numbers = calculateAndRemoveCustomSeparators(numbers);
+
+        String[] arrayNumbers = numbers.split(separators);
+        for (String number : arrayNumbers) {
+            int numberInt = Integer.valueOf(number);
+            if (numberInt < 0) {
+                negatives.add(numberInt);
+            }
+            result += numberInt;
+        }
+
+        validateNegatives(negatives);
+
         return result;
     }
 
-    private String calculateSeparatorsCustom(String numbers) {
-        if(numbers.startsWith("//")) {
+    private void validateNegatives(List<Integer> negatives) {
+        if (!negatives.isEmpty()) {
+            StringBuffer stringBuffer = new StringBuffer("Error! Negatives not allowed: ");
+            for (Integer negative : negatives) {
+                stringBuffer.append(negative);
+                stringBuffer.append(" ");
+            }
+
+            throw new IllegalArgumentException(stringBuffer.toString());
+        }
+    }
+
+    private String calculateAndRemoveCustomSeparators(String numbers) {
+        if (numbers.startsWith("//")) {
             String customSeparator = numbers.substring(2, 3);
 
             separators = separators.concat("|");
             separators = separators.concat(customSeparator);
 
-            numbers = numbers.substring(4, numbers.length());
+            numbers = numbers.substring(4);
         }
 
         return numbers;
--- src/test/java/StringCalculatorShould.java
+++ src/test/java/StringCalculatorShould.java
@@ -1,51 +1,63 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class StringCalculatorShould {
     StringCalculator stringCalculator = new StringCalculator();
 
     @Test
     public void be_0_when_numbers_are_empties() {
-        Assert.assertEquals(0, stringCalculator.add(""));
+        assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
     public void be_4_when_send_just_one_number_and_it_is_4() {
-        Assert.assertEquals(4, stringCalculator.add("4"));
+        assertEquals(4, stringCalculator.add("4"));
     }
 
     @Test
     public void be_6_when_send_just_one_number_and_it_is_6() {
-        Assert.assertEquals(6, stringCalculator.add("6"));
+        assertEquals(6, stringCalculator.add("6"));
     }
 
     @Test
     public void be_7_when_send_4_and_3() {
-        Assert.assertEquals(7, stringCalculator.add("4,3"));
+        assertEquals(7, stringCalculator.add("4,3"));
     }
 
     @Test
     public void be_2_when_send_0_and_2() {
-        Assert.assertEquals(2, stringCalculator.add("0,2"));
+        assertEquals(2, stringCalculator.add("0,2"));
     }
 
     @Test
     public void be_6_when_send_1_and_2_and_3() {
-        Assert.assertEquals(6, stringCalculator.add("1,2,3"));
+        assertEquals(6, stringCalculator.add("1,2,3"));
     }
 
     @Test
     public void be_6_when_send_1_and_2_and_3_with_new_separator() {
-        Assert.assertEquals(6, stringCalculator.add("1\n2,3"));
+        assertEquals(6, stringCalculator.add("1\n2,3"));
     }
 
     @Test
     public void be_28_when_send_1_to_7_with_new_separator() {
-        Assert.assertEquals(28, stringCalculator.add("1\n2,3\n4,5,6\n7"));
+        assertEquals(28, stringCalculator.add("1\n2,3\n4,5,6\n7"));
     }
 
     @Test
     public void be_28_with_custom_separator() {
-        Assert.assertEquals(28, stringCalculator.add("//;\n1;2;3;4;5;6;7"));
+        assertEquals(28, stringCalculator.add("//;\n1;2;3;4;5;6;7"));
+    }
+
+    @Test
+    public void be_28_with_another_custom_separator() {
+        assertEquals(28, stringCalculator.add("//ª\n1ª2ª3ª4ª5ª6ª7"));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void throw_exception_with_negatives() {
+        stringCalculator.add("1,-2,-3");
     }
 }
